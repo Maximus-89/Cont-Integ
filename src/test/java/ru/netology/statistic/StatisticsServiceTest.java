2c4bf51159b6070236a5e7b527eadecd2524e048
Maximus <maxis30.09@mail.ru>
2023-04-11 13:41:13 +0300

    src/test/java/ru/netology/statistic/StatisticsServiceTest.java

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,11 +18,11 @@ public class StatisticsServiceTest {
         Assertions.assertEquals(expected, actual);
     }
     @Test
-    void findMiddle() {
+    void findMaxInTheMiddle() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 39;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 30, 8, 6, 11, 11, 12};
+        long expected = 30;
 
         long actual = service.findMax(incomesInBillions);
 
